683283e0cfa4dff61912a74c7d52bdf87c35410a
Lucas Ezio Fraga <lucas.fraga@gmail.com>
2020-11-07 21:17:36 -0300

    EsteVaJoya

--- Proyecto/Formularios/seleccionestados.Designer.cs
+++ Proyecto/Formularios/seleccionestados.Designer.cs
@@ -28,33 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.estadosDisponibles = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.estadosSeleccionados = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvEstadosD = new System.Windows.Forms.DataGridView();
+            this.Seleccion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvEstadosS = new System.Windows.Forms.DataGridView();
+            this.SeleccionSel = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.EstadoSel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadosD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadosS)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.AllowUserToDeleteRows = false;
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.estadosDisponibles});
-            this.dataGridView2.Location = new System.Drawing.Point(93, 166);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.ReadOnly = true;
-            this.dataGridView2.RowHeadersVisible = false;
-            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView2.Size = new System.Drawing.Size(270, 187);
-            this.dataGridView2.TabIndex = 1;
-            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // button1
             // 
@@ -74,6 +61,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "ELIMINAR";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -93,71 +81,96 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "ELIMINAR TODO";
             this.button4.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.estadosSeleccionados});
-            this.dataGridView1.Location = new System.Drawing.Point(490, 166);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(270, 187);
-            this.dataGridView1.TabIndex = 6;
-            // 
-            // estadosDisponibles
-            // 
-            this.estadosDisponibles.Frozen = true;
-            this.estadosDisponibles.HeaderText = "ESTADOS DISPONIBLES";
-            this.estadosDisponibles.Name = "estadosDisponibles";
-            this.estadosDisponibles.ReadOnly = true;
-            this.estadosDisponibles.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.estadosDisponibles.Width = 200;
-            // 
-            // estadosSeleccionados
-            // 
-            this.estadosSeleccionados.Frozen = true;
-            this.estadosSeleccionados.HeaderText = "ESTADOS DISPONIBLES";
-            this.estadosSeleccionados.Name = "estadosSeleccionados";
-            this.estadosSeleccionados.ReadOnly = true;
-            this.estadosSeleccionados.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.estadosSeleccionados.Width = 200;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // backgroundWorker1
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // dgvEstadosD
+            // 
+            this.dgvEstadosD.AllowUserToAddRows = false;
+            this.dgvEstadosD.AllowUserToDeleteRows = false;
+            this.dgvEstadosD.AllowUserToResizeColumns = false;
+            this.dgvEstadosD.AllowUserToResizeRows = false;
+            this.dgvEstadosD.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEstadosD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEstadosD.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Seleccion,
+            this.Estado});
+            this.dgvEstadosD.Location = new System.Drawing.Point(71, 176);
+            this.dgvEstadosD.Name = "dgvEstadosD";
+            this.dgvEstadosD.RowHeadersVisible = false;
+            this.dgvEstadosD.Size = new System.Drawing.Size(254, 177);
+            this.dgvEstadosD.TabIndex = 6;
+            // 
+            // Seleccion
+            // 
+            this.Seleccion.HeaderText = "Seleccion";
+            this.Seleccion.Name = "Seleccion";
+            // 
+            // Estado
+            // 
+            this.Estado.HeaderText = "Estado";
+            this.Estado.Name = "Estado";
+            // 
+            // dgvEstadosS
+            // 
+            this.dgvEstadosS.AllowUserToAddRows = false;
+            this.dgvEstadosS.AllowUserToDeleteRows = false;
+            this.dgvEstadosS.AllowUserToResizeColumns = false;
+            this.dgvEstadosS.AllowUserToResizeRows = false;
+            this.dgvEstadosS.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEstadosS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEstadosS.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SeleccionSel,
+            this.EstadoSel});
+            this.dgvEstadosS.Location = new System.Drawing.Point(514, 176);
+            this.dgvEstadosS.Name = "dgvEstadosS";
+            this.dgvEstadosS.RowHeadersVisible = false;
+            this.dgvEstadosS.Size = new System.Drawing.Size(254, 177);
+            this.dgvEstadosS.TabIndex = 7;
+            // 
+            // SeleccionSel
+            // 
+            this.SeleccionSel.HeaderText = "Seleccion";
+            this.SeleccionSel.Name = "SeleccionSel";
+            // 
+            // EstadoSel
+            // 
+            this.EstadoSel.HeaderText = "Estado";
+            this.EstadoSel.Name = "EstadoSel";
             // 
             // seleccionestados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvEstadosS);
+            this.Controls.Add(this.dgvEstadosD);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView2);
             this.Name = "seleccionestados";
             this.Text = "seleccionestados";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadosD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadosS)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn estadosDisponibles;
-        private System.Windows.Forms.DataGridViewTextBoxColumn estadosSeleccionados;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dgvEstadosD;
+        private System.Windows.Forms.DataGridView dgvEstadosS;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Seleccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn SeleccionSel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EstadoSel;
     }
 }
--- Proyecto/Formularios/seleccionestados.cs
+++ Proyecto/Formularios/seleccionestados.cs
@@ -15,34 +15,111 @@ namespace Proyecto.Formularios
         public seleccionestados()
         {
             InitializeComponent();
-            dataGridView2.Rows.Add("Estado 1");
-            dataGridView2.Rows.Add("Estado 2");
-            dataGridView2.Rows.Add("Estado 3");
+            dgvEstadosD.Rows.Add(false, "Estado 1");
+            dgvEstadosD.Rows.Add(false, "Estado 2");
+            dgvEstadosD.Rows.Add(false, "Estado 3");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            foreach (DataGridViewRow row in dataGridView2.SelectedRows )
-            {
-                dataGridView1.Rows.Add(dataGridView2);
+            //
+            // Se define una lista temporal de registro seleccionados
+            //
+            List<DataGridViewRow> rowSelected = new List<DataGridViewRow>();
 
+            //
+            // Se recorre ca registro de la grilla de origen
+            //
+            foreach (DataGridViewRow row in dgvEstadosD.Rows)
+            {
+                //
+                // Se recupera el campo que representa el checkbox, y se valida la seleccion
+                // agregandola a la lista temporal
+                //
+                DataGridViewCheckBoxCell cellSelecion = row.Cells["Seleccion"] as DataGridViewCheckBoxCell;
+
+                if (Convert.ToBoolean(cellSelecion.Value))
+                {
+                    rowSelected.Add(row);
+                }
             }
 
+            //
+            // Se agrega el item seleccionado a la grilla de destino
+            // eliminando la fila de la grilla original
+            //
+            foreach (DataGridViewRow row in rowSelected)
+            {
+                dgvEstadosS.Rows.Add(new object[] {false,
+                                            row.Cells["Estado"].Value });
+
+                dgvEstadosD.Rows.Remove(row);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            foreach (DataGridViewRow row in dgvEstadosD.Rows)
+            {
 
+                row.Cells["Seleccion"].Value = true;
+
+            }
+            button1.PerformClick();
         }
 
-        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //
+            // Se define una lista temporal de registro seleccionados
+            //
+            List<DataGridViewRow> rowSelected = new List<DataGridViewRow>();
+
+            //
+            // Se recorre ca registro de la grilla de origen
+            //
+            foreach (DataGridViewRow row in dgvEstadosS.Rows)
+            {
+                //
+                // Se recupera el campo que representa el checkbox, y se valida la seleccion
+                // agregandola a la lista temporal
+                //
+                DataGridViewCheckBoxCell cellSelecion = row.Cells["SeleccionSel"] as DataGridViewCheckBoxCell;
+
+                if (Convert.ToBoolean(cellSelecion.Value))
+                {
+                    rowSelected.Add(row);
+                }
+            }
+
+            //
+            // Se agrega el item seleccionado a la grilla de destino
+            // eliminando la fila de la grilla original
+            //
+            foreach (DataGridViewRow row in rowSelected)
+            {
+                dgvEstadosD.Rows.Add(new object[] {false,
+                                            row.Cells["EstadoSel"].Value });
+
+                dgvEstadosS.Rows.Remove(row);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgvEstadosS.Rows)
+            {
+
+                row.Cells["SeleccionSel"].Value = true;
+
+            }
+            button2.PerformClick();
         }
     }
 }
